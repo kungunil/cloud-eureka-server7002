5f71986c232aa5014bb28278741385d88bfdda9f
kungunil <9091923224@qq.com>
2021-08-28 17:30:57 +0800

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 黄龙
+ * @Date: 2021/08/28/17:26
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
